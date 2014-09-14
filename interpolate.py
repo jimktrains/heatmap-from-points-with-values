bd4f45be5b9d5c9292df61c39b1fe958be8a4078
Jim Keener <jim@jimkeener.com>
2014-09-13 20:20:03 -0400

    adds comments and cleans up dead code

--- interpolate.py
+++ interpolate.py
@@ -5,142 +5,104 @@ from osgeo import osr, gdal
 import math
 import csv
 
-import collections
-import functools
-
 rows = []
 
 
 # Set file vars
 output_file = "out.tif"
 
-# Create gtif
-driver = gdal.GetDriverByName("GTiff")
 
 max_lon = -79.96990
-max_lat = 40.430
+max_lat =  40.43090
  
 min_lon = -80.00625
-min_lat = 40.40250
+min_lat =  40.40250
 
 
-res = 0.0001
+sq_size = 5
+deg_per_block = 0.0001
 
-x_size = int((max_lon - min_lon) / res)
-y_size = int((max_lon - min_lon) / res) 
-dst_ds = driver.Create(output_file, x_size, y_size, 1, gdal.GDT_Byte )
-raster = np.zeros( (x_size, y_size) ) 
-final_raster = np.zeros( (x_size, y_size) ) 
-cntr = np.zeros( (x_size, y_size) ) 
-
-
-with open("../allentown_heatmap_spreadsheet.csv") as csv_file:
-   csvfr = csv.DictReader(csv_file)
-   for row in csvfr:
-      row['Lat'] = float(row['Lat'])
-      row['Lon'] = float(row['Lon'])
-      row['Strength'] = 10*float(row['Strength'])
-
-      rows.append(row)
-
-#max_val = sorted([row['Strength'] for i in range(len(rows))])
-#max_val = filter(lambda x : x != 0, max_val)
-#s = len(max_val)
-#max_val = max_val[int(s - (s*0.05))]
-#
-#for i in xrange(len(rows)):
-#   r = rows[i]
-#   r['Strength'] = r['Strength'] / max_val
-#   rows[i] = r
-
-sigma = 5
-
-m = 1./math.hypot(sigma, sigma)
-b = 1
+# Compute the image size in blocks
+lon_size = int((max_lon - min_lon) / deg_per_block)
+lat_size = int((max_lat - min_lat) / deg_per_block) 
 
 def decay(x):
     return math.pow(0.75, x)
-    return (-m*x) + b
-    return math.cos(x * m/2. * math.pi / 4)
-    return 1 - 1 / (1 + math.exp((x-(sigma/2.))))
-
 
 def x(lon):
-   return int((lon - min_lon)/res)
+    return int((lon - min_lon)/deg_per_block)
 def y(lat):
-   return int((lat - min_lat)/res)
+    return int((lat - min_lat)/deg_per_block)
+
+with open("../allentown_heatmap_spreadsheet.csv") as csv_file:
+    csvfr = csv.DictReader(csv_file)
+    for row in csvfr:
+        row['Lat'] = float(row['Lat'])
+        row['Lon'] = float(row['Lon'])
+        row['Strength'] = float(row['Strength'])
+
+        rows.append(row)
 
 
-processed = []
-cntr = np.zeros( (x_size, y_size) ) 
+processed = {}
+cntr = {}
 for row in rows:
-  i = x(float(row['Lon']))
-  j = y(float(row['Lat']))
-  n = ((cntr[j,i] * raster[j,i]) + row['Strength']) / (1 + cntr[j,i]);
-  raster[j,i] = n
-  cntr[j,i] += 1
-  pair = (j,i)
-  if pair not in processed: processed.append(pair)
+    lon = x(float(row['Lon']))
+    lat = y(float(row['Lat']))
 
-values = []
-for pair in processed:
-  (j,i) = pair
-  values.append(raster[j,i])
+    idx = (lon,lat)
 
-values = sorted(values)
+    if idx not in processed:
+        processed[idx] = 0
+        cntr[idx] = 0
 
-ptile = 20
-ptiles = []
-s = len(values)
-for i in range(ptile-1):
-   ptiles.append(values[int(s * float(1)/float(ptile) * (i+1))])
-#print ptiles
+    # Average all readings that belong in this cell
+    n = ((cntr[idx] * processed[idx]) + row['Strength']) / (1 + cntr[idx]);
+    processed[idx] = n
+    cntr[idx] += 1
 
-for pair in processed:
-  (j,i) = pair
-  qile = ptile
-  for q in range(len(ptiles)):
-    if ptiles[q] > raster[j,i]:
-      qile = q
-      break 
-  raster[j,i] = 10 + ((100./ptile)*qile)
 
-cntr = np.zeros( (x_size, y_size) ) 
+# So why are we using lat, lon for matrix indecis?
+# Good question
+# In a matrix, the first index refers to rows; the second to the column
+# i.e rows get bigger as you move "down" them 
+#     and columns get bigger as you move "across" them
+# as such, they are interpreted as y-coordinates and then x-coordinates
+
+cntr = np.zeros( (lat_size, lon_size) ) 
+raster = np.zeros( (lat_size, lon_size) ) 
 cnt = 0
-for pair in processed:
-  cnt += 1
-  if cnt % 150 == 0: print cnt
-  (rj, ri) = pair
-  for i in range(max(ri-sigma,0), min(ri+sigma, x_size-1)):
-     for j in range(max(rj-sigma,0), min(rj+sigma, y_size-1)):
-         #if i == ri and j == rj: continue
-         dx = abs(ri-i)
-         dy = abs(rj-j)
-         r = math.hypot(dx,dy)
-         g = decay(r)
-         s = raster[rj,ri]
-         v = g * s
-         #print dx,dy,r,s,g,v
-         #raster[j,i] += v
+for lon,lat in processed:
+    cnt += 1
+    if cnt % 150 == 0: print cnt
 
-         #raster[j,i] = max(raster[j,i], v)
-         #v = (raster[j,i] + v)/2
-         #raster[j,i] = raster[j,i] + v
-         final_raster[j,i] = ((cntr[j,i]*final_raster[j,i]) + v) / (1 + cntr[j,i])
-         cntr[j,i] += 1
+    # Loop through the quare size around the reading and add the reading's
+    # decay values to the raster
 
-#final_raster += raster
+    for j in range(max(lat-sq_size,0), min(lat+sq_size, lat_size-1)):
+        for i in range(max(lon-sq_size,0), min(lon+sq_size, lon_size-1)):
+            r = math.hypot(j-lat, i-lon)
+            v = decay(r) * processed[(lon,lat)]
 
-print final_raster.max();
+            # Computes the running average for the current value of the cell
+            n = ((cntr[j,i]*raster[j,i]) + v) / (1 + cntr[j,i])
+            raster[j,i] = n
+            cntr[j,i] += 1
 
-#max_val = final_raster.max()
-#final_raster /=  max_val
-#final_raster *=  200
-#
-#print final_raster.max()
+# Normalize valus from 0 to 255
+max_val = raster.max()
+raster /=  max_val
+raster *=  255
 
-# top left x, w-e pixel resolution, rotation, top left y, rotation, n-s pixel resolution
-dst_ds.SetGeoTransform( [ min_lon, res, 0, min_lat, 0, res ] )
+# Create gtif
+driver = gdal.GetDriverByName("GTiff")
+dst_ds = driver.Create(output_file, lon_size, lat_size, 1, gdal.GDT_Byte )
+
+# Don't ask, I don't know why the order is like this
+# top left x, w-e pixel resolution, rotation, 
+# top left y, rotation, n-s pixel resolution
+dst_ds.SetGeoTransform([ min_lon, deg_per_block, 0, 
+                         min_lat, 0, deg_per_block])
   
 # set the reference info 
 srs = osr.SpatialReference()
@@ -148,4 +110,4 @@ srs.SetWellKnownGeogCS("WGS84")
 dst_ds.SetProjection( srs.ExportToWkt() )
 
 # write the band
-dst_ds.GetRasterBand(1).WriteArray(final_raster)
+dst_ds.GetRasterBand(1).WriteArray(raster)
